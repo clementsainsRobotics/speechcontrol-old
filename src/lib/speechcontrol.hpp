1880135d4ec93b880f789f5938eb69097adf276e
Jacky Alcine <me@jalcine.me>
2012-09-28 21:48:15 -0400

    Added a core SpeechControl file so that people could see the magic of SpeechControl in one fell swoop.

---
+++ src/lib/speechcontrol.hpp
@@ -0,0 +1,47 @@
+/***
+ *  This file is part of SpeechControl.
+ *
+ *  Copyright (C) 2012 Adrian Borucki <adrian.borucki@thesii.org>
+ *                2012 Jacky Alcine <jacky.alcine@thesii.org>
+ *
+ *  SpeechControl is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  SpeechControl is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with SpeechControl .  If not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef SPEECHCONTROL_LIB
+#define SPEECHCONTROL_LIB
+
+#include <lib/global.hpp>
+#include <lib/decoder.hpp>
+#include <lib/languagemodel.hpp>
+#include <lib/acousticmodel.hpp>
+#include <lib/dictionary.hpp>
+#include <lib/noisedictionary.hpp>
+
+/**
+ * @author Jacky Alciné <jacky.alcine@thesii.org>
+ * @date September 28, 2012 9:24 PM GMT-5
+ *
+ * @mainpage SpeechControl Library
+ * @section intro Introduction
+ *
+ * SpeechControl is the attempt by the Synthetic Intellect Institute to provide
+ * a unifed base of speech recognition technologies from multiple groups like
+ * CMU's PocketSphinx and the Julius group (tba) into one cross-platform and
+ * easy to use solution. With no frills and more flexibility, this library is
+ * designed to make your developing life a bit easier to have with some of the
+ * magic tools of speech recognition and computation that's involved with it.
+ */
+
+#endif /* SPEECHCONTROL_LIB */
