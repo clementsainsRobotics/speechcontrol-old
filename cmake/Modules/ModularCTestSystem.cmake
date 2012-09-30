91e085fe01e5a53f3820a53b1e1703ed89e535b7
Jacky Alcine <me@jalcine.me>
2012-09-30 19:03:30 -0400

    A bit more additions to the testing system.

--- cmake/Modules/ModularCTestSystem.cmake
+++ cmake/Modules/ModularCTestSystem.cmake
@@ -2,7 +2,16 @@
 #
 # This module provides developers and testers alike with a nifty way of testing
 # applications with multiple sources. This allows for quick integration with
-# systems that love to do a lot of unit testing.s
+# systems that love to do a lot of unit testing.
+#
+# The following variables MUST be defined before including this module:
+#
+# - CTEST_META_TEMPLATE: The template file that'd be used to render all of the tests.
+# - CTEST_META_TEST_DIR: The directory that contains all of the sources to be used as
+#       core files for the template.
+# - CTEST_META_TEST_SUFFIX: The pattern of the file to look for. Defaults to (*.cpp)
+# - CTEST_META_CONFIG_DIR: The directory that contains additionial configuration information
+#       for each test.
 #
 #=============================================================================
 # Copyright (c) 2012 Jacky Alcine <jacky.alcine@thesii.org>
@@ -21,6 +30,10 @@ cmake_minimum_required(VERSION 2.8)
 include(CTest)
 
 if (BUILD_TESTING EQUAL ON)
+    # Check if the suffix regex is set.
+    if (NOT DEFINED CTEST_META_TEST_SUFFIX)
+        set(CTEST_META_TEST_SUFFIX "*.cpp")
+    endif(NOT DEFINED CTEST_META_TEST_SUFFIX)
 
     # Add a meta-target for all tests.
     add_custom_target(tests)
@@ -28,7 +41,17 @@ if (BUILD_TESTING EQUAL ON)
     # Load the testing template.
     if (NOT DEFINED CTEST_TEMPLATE)
         message(SEND_ERROR "No template was defined in CTEST_TEMPLATE to use for the test executables!")
-    elseif(NOT DEFINED CTEST_TEMPLATE)
-        
     endif(NOT DEFINED CTEST_TEMPLATE)
+    file(READ ${CTEST_TEMPLATE} _CTEST_BUILD_TEMPLATE)
+
+    # Set up build directory.
+    set(_test_cmake_output_dir "${CMAKE_CURRENT_BINARY_DIR}")
+    message(STATUS ${_test_cmake_output_dir})
+
+    # Loop over each file for testing.
+    file(GLOB _test_file ${CTEST_META_TEST_DIR} ${CTEST_META_TEST_SUFFIX})
+    foreach(_test_file ${_test_files})
+
+    endforeach(_test_file ${_test_files})
+
 endif(BUILD_TESTING EQUAL ON)
