a18b4a29363883dab2b3d0582dab63dfb0967e18
Jacky Alcine <me@jalcine.me>
2012-10-01 05:13:50 -0400

    Upgrading the testing platform. It's ready for show-time.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -4,12 +4,14 @@
 set(CTEST_PROJECT_NAME "SpeechControl")
 set(CTEST_NIGHTLY_START_TIME "06:00:00 UTC")
 
-set(CTEST_DROP_METHOD     "http")
-set(CTEST_DROP_SITE       "developers.thesii.org")
-set(CTEST_DROP_LOCATION   "/testing/dashboard/submit.php?project=${CTEST_PROJECT_NAME}")
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "cdash.thesii.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=SpeechControl")
 set(CTEST_DROP_SITE_CDASH TRUE)
+
 
 set(CTEST_OUTPUT_ON_FAILURE TRUE)
 set(CTEST_UPDATE_COMMAND "git pull")
 set(CTEST_SOURCE_DIRECTORY "${CMAKE_SOURCE_DIR}")
 set(CTEST_BINARY_DIRECTORY "${CMAKE_BINARY_DIR}")
+
