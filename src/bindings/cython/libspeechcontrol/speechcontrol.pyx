894d57860b357bb9795ae05ae40e1946adb55a45
Adrian Borucki <gentoolx@gmail.com>
2012-11-02 20:58:53 +0100

    Fix a QDir bug crashing Cython.

--- src/bindings/cython/libspeechcontrol/speechcontrol.pyx
+++ src/bindings/cython/libspeechcontrol/speechcontrol.pyx
@@ -19,15 +19,15 @@ cdef class LanguageModel:
     def __dealloc__(self):
         del self.cSelf
 
-#    @staticmethod
-#    def fromDirectory(char *path):
-#        cdef C_LanguageModel *newModel
-        #newModel = fromDirectory(QDir(QString(path))) <<< This line crashes Cython o.O
-#        wrapper = LanguageModel()
+    @staticmethod
+    def fromDirectory(char *path):
+        cdef C_LanguageModel *newModel
+        newModel = fromDirectory(QDir(QString(path)))
+        wrapper = LanguageModel()
 
-#        del wrapper.cSelf # A little bit ugly but should work.
-#        wrapper.cSelf = newModel
-#        return wrapper
+        del wrapper.cSelf # A little bit ugly but should work.
+        wrapper.cSelf = newModel
+        return wrapper
 
 cdef class Dictionary:
     cdef C_Dictionary *cSelf
