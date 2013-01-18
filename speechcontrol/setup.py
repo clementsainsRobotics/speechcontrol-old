a5422f5e327e896e42d7c6ebb2dec04fb27cd8a3
Adrian Borucki <gentoolx@gmail.com>
2013-01-18 12:12:26 +0100

    Update INSTALL file.

--- speechcontrol/setup.py
+++ speechcontrol/setup.py
@@ -2,6 +2,7 @@ from distutils.core import setup
 setup(
         name="SpeechControl",
         version="0.1",
+        description="control your computer with voice",
         author="SpeechControl Developers",
         author_email="dev@thesii.org",
         url="http://www.thesii.org/projects/speechcontrol.html",
