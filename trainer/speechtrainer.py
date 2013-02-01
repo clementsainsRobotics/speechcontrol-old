201a59caf4a49eb5a86c31673291e1a2b7c6176e
Adrian Borucki <gentoolx@gmail.com>
2013-02-01 18:01:37 +0100

    Polish SpeechTrainer a bit.

--- trainer/speechtrainer.py
+++ trainer/speechtrainer.py
@@ -8,36 +8,34 @@ import gi
 gi.require_version('Gst', '0.10')
 from gi.repository import Gst
 
-from dbus.mainloop.qt import DBusQtMainLoop
-from PyQt4 import QtDBus
+from PyQt4 import QtCore, QtDBus
 from PyQt4.QtDBus import QDBusConnection
 from PyQt4.QtGui import QApplication
 
-from app import App
+#from app import App
+from speechcontrol.asr import SpeechControl
 
 
 # parser.add_argument("--wsupport", action="store_true", help="enable Wintermute support")
 parser = argparse.ArgumentParser()
-parser.add_argument("-c", "--config", help="configuration file to use")
+parser.add_argument("-c", "--config", help="configuration file to use",
+                    default="/usr/share/speechcontrol/config/default.conf")
 parser.add_argument("-a", "--asr", help="automatic speech recognition backend to use",
-                    choices=["pocketsphinx"])
+                    choices=["pocketsphinx"], default="pocketsphinx")
 args = parser.parse_args()
 
-DEFAULT_CONFIG = "/usr/share/speechcontrol/config/default.conf"
-DEFAULT_ASR = "pocketsphinx"
-
 # Try to load Wintermute support (Agent class, etc.)
-# if WINTERMUTE_SUPPORT:
+# if args.wntrsupport:
 #       try:
 #               from wintermute.members import Agent
 #       except:
 #               print("[I] Wintermute support not found.")
-#               WINTERMUTE_SUPPORT = False
+#               args.wntrsupport = False
 # else:
 #       print("[I] Running as a standalone application.")
 
 
-def read_configuration(file=DEFAULT_CONFIG):
+def read_configuration(file):
         pass
 
 
@@ -45,19 +43,14 @@ def run():
     # Establish D-Bus
     # Instantiate every important class
     # Run!
-    app = App(sys.argv)
-    app.set_asr_backend(DEFAULT_ASR)
+    app = QApplication(sys.argv)
 
-    DBusQtMainLoop(set_as_default=True)
-    bus = QDBusConnection.sessionBus()
+    sc = SpeechControl()
+    sc.setAsrBackend(args.asr)
 
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
     read_configuration(args.config)
-
-    # Overwrite config by command-line arguments
-    if args.asr:
-        DEFAULT_ASR = args.asr
-
     run()
+
