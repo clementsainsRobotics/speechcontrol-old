48f6d5412768e5f771c5f0c8a2abdd95119720f7
Adrian Borucki <gentoolx@gmail.com>
2013-03-08 21:09:52 +0100

    Introduce PS backends wrapper

--- daemon/asr/backend.py
+++ daemon/asr/backend.py
@@ -5,7 +5,7 @@ from gi.repository import GObject, Gst
 GObject.threads_init()
 Gst.init(None)
 
-class PocketSphinx:
+class GstPocketSphinx:
     def __init__(self):
         self.pipeline = Gst.parse_launch('autoaudiosrc ! audioconvert ! audioresample '
                                          + '! vader name=vad auto-threshold=true '
@@ -28,4 +28,22 @@ class PocketSphinx:
         pass
 
     def application_message(self):
+        pass
+
+class NativePocketSphinx:
+    def __init__(self):
+        pass
+
+"""
+A wrapper backend supporting the choice of the two above ones.
+Availability of each will depend on their state of implementation.
+
+For now, the case is that GStreamer does not work for us becuase there is not
+a PocketSphinx plug-in for the 1.0 version.
+Alternative approach is being implemented based on using PocketSphinx Python
+bindings. It should at least support singular utterance recognition using user's
+microphone.
+"""
+class PocketSphinx:
+    def __init__(self):
         pass
