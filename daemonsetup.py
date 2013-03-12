ff3e3f8e7c90463fbb129c0b156960b71678715d
Adrian Borucki <gentoolx@gmail.com>
2013-03-12 13:37:20 +0100

    Move extension configuration to the main script

--- daemonsetup.py
+++ daemonsetup.py
@@ -1,4 +1,24 @@
 from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+include_dirs = [
+        "/usr/include/sphinxbase",
+        "/usr/include/pocketsphinx"
+        ]
+
+libraries = [
+        "pocketsphinx",
+        "sphinxad"
+        ]
+
+ext_modules = [
+        Extension("pocketsphinx", ["daemon/asr/pocketsphinx/pocketsphinx.pyx"],
+            include_dirs=include_dirs,
+            libraries=libraries
+            )
+        ]
+
 
 package_dirs = {"" : "daemon"}
 setup(
@@ -11,5 +31,7 @@ setup(
         license="GPLv3+",
         package_dir=package_dirs,
         packages=["asr"],
-        scripts=["daemon/speechdaemon.py"]
+        scripts=["daemon/speechdaemon.py"],
+        cmdclass = {'build_ext': build_ext},
+        ext_modules = ext_modules
         )
