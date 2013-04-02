57f9d5b8db26ae8bbc0ad27770d54387ffdad929
Adrian Borucki <gentoolx@gmail.com>
2013-04-02 20:47:28 +0200

    Fix oneUtterance() method

--- package/speechcontrol/recognition.py
+++ package/speechcontrol/recognition.py
@@ -6,34 +6,31 @@ DBUS_OBJECT_PATH = "/SpeechRecognizer"
 DBUS_INTERFACE = "org.sii.speechcontrol.SpeechRecognition"
 
 class RecognitionInterface(QtDBus.QDBusAbstractInterface):
-	def __init__(self, service, path, connection, parent=None):
-		super().__init__(service, path, DBUS_INTERFACE, connection, parent)
+    def __init__(self, service, path, connection, parent=None):
+        super().__init__(service, path, DBUS_INTERFACE, connection, parent)
 
-	def oneUtterance(self):
-		reply = self.call("oneUtterance")
-		if reply.isValid():
-			return reply.arguments()[0]
-		else:
-			return ""
+    def oneUtterance(self):
+        msg = self.call("oneUtterance")
+        return msg.arguments()[0]
 
     def oneUtteranceAsync(self):
         self.callAsync("oneUtterance")
 
-	def oneUtteranceTo(self, receiver, method, errMethod):
-		self.callWithCallback("oneUtterance", [], receiver, method, errMethod)
+    def oneUtteranceTo(self, receiver, method, errMethod):
+        self.callWithCallback("oneUtterance", [], receiver, method, errMethod)
 
 class AsrFacility(QtCore.QObject):
-	def __init__(self):
-		super().__init__()
-		self.recogIface = RecognitionInterface(DBUS_SERVICE, DBUS_OBJECT_PATH, 
-			QtDBus.QDBusConnection.sessionBus(), self)
+    def __init__(self):
+        super().__init__()
+        self.recogIface = RecognitionInterface(DBUS_SERVICE, DBUS_OBJECT_PATH, 
+            QtDBus.QDBusConnection.sessionBus(), self)
 
-	def requestUtterance(self):
-		"""Request one utterance from SpeechControl
+    def requestUtterance(self):
+        """Request one utterance from SpeechControl
 
-			This call is a blocking one, use it if you need the text immediately.
-		"""
-		return self.recogIface.oneUtterance()
+            This call is a blocking one, use it if you need the text immediately.
+        """
+        return self.recogIface.oneUtterance()
 
     def requestUtteranceAsync(self):
         """Request one utterance from SpeechControl
