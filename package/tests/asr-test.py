91e544cf0d797e0c5a4e0d3b4d8d08f21118f85b
Adrian Borucki <gentoolx@gmail.com>
2013-05-04 00:17:44 +0200

    Try to test asynchronous speech recognition

--- package/tests/asr-test.py
+++ package/tests/asr-test.py
@@ -1,13 +1,19 @@
 import sys
 
+_utt = ""
+
 def setup():
     sys.path.append('..')
+
+def callback(utt):
+    print("*** Recognized text:", utt)
+    _utt = utt
 
 def asr_test():
     from speechcontrol.recognition import AsrFacility
     a = AsrFacility()
-    print("*** Listening for one utterance")
-    utt = a.requestUtterance()
-    print("*** Recognized text:", utt)
+    a.addReceiver(callback)
 
-    assert(utt != "")
+    print("*** Listening for one utterance")
+
+    assert(_utt != "")
